a15f85090fecdd7d57555583905ba61f2d2343af
snorlax <68203941+snorlax93@users.noreply.github.com>
2021-12-23 06:30:02 -0500

    frontend
    using ddns.net address instead of "localhost", i suspect that's the issue. getting a 500 error.

--- frontend/src/services/AccountServices.js
+++ frontend/src/services/AccountServices.js
@@ -3,7 +3,7 @@ import axios from "axios";
 class AccountService {
     
     getLoginAccounts() {
-        return axios.get("http://localhost:8080/api/v1/account/login");
+        return axios.get("http://snorlaxx-server.ddns.net:8080/api/v1/account/login");
     }
 }
 
