9e5b3688672e3e92c41655c39a3c8e8790b3802e
snorlax <68203941+snorlax93@users.noreply.github.com>
2021-12-23 06:26:22 -0500

    backend update
    trying a global cors rule for testing on snorlaxx-server

--- backend/src/main/java/com/rathena/online/controller/AccountController.java
+++ backend/src/main/java/com/rathena/online/controller/AccountController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/account/")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class AccountController {
 
     @Autowired
