a0e248ad8b896f8b82508676ef1ae9dcca18b78b
snorlax <68203941+snorlax93@users.noreply.github.com>
2021-12-23 22:42:18 -0500

    backend updates
    update to login repository and Account controller

--- backend/src/main/java/com/rathena/online/controller/AccountController.java
+++ backend/src/main/java/com/rathena/online/controller/AccountController.java
@@ -1,9 +1,15 @@
 package com.rathena.online.controller;
 
+import com.rathena.online.exception.ResourceNotFoundException;
 import com.rathena.online.model.Login;
 import com.rathena.online.repository.LoginRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,10 +24,52 @@ public class AccountController {
     private LoginRepository loginRepository;
 
     /*
-     * get all login accounts
+     * This method will get every available login record
+     * 
+     * @return array of objects
      */
     @RequestMapping("/login")
     public List<Login> getAllAccounts() {
         return loginRepository.findAll();
+    }
+    
+
+    /*
+     * This method will create 1 login record
+     * 
+     * @params
+     * @return
+     */
+    @PostMapping("/login/new")
+    public Login createLogin(@RequestBody Login account) {
+        return loginRepository.save(account);
+    }
+
+    /*
+     * This method will get 1 (single) login record based on id
+     * 
+     * @params
+     * @return
+     */
+    @RequestMapping("/login/find/userid/{userId}")
+    public ResponseEntity<Login> getAccountByUserId(@PathVariable String userId) {
+        Login account = loginRepository.getByUserid(userId).orElseThrow(
+            ()-> new ResourceNotFoundException("Account not found with User ID: " + userId)
+        );
+        return ResponseEntity.ok(account);
+    }
+
+    /*
+     * This method will get 1 (single) login record based on id
+     * 
+     * @params
+     * @return
+     */
+    @RequestMapping("/login/id/{id}")
+    public ResponseEntity<Login> getAccountById(@PathVariable int id) {
+        Login account = loginRepository.findById(id).orElseThrow(
+            ()-> new ResourceNotFoundException("Account not found with ID: " + id)
+        );
+        return ResponseEntity.ok(account);
     }
 }
